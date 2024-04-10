ca7e6b4b7d252dcb3f0fa9418639f51e26e72866
Kumar Mohit <mohit724196@gmail.com>
2024-04-11 00:59:36 +0530

    code

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFb4n0mrMrxP1zbInNYYXXS2gng3IhZJk",
+  authDomain: "yt-clone-323f9.firebaseapp.com",
+  projectId: "yt-clone-323f9",
+  storageBucket: "yt-clone-323f9.appspot.com",
+  messagingSenderId: "218978686795",
+  appId: "1:218978686795:web:028b2114a4904013403ace"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
